cfdb014d8861cb223523bbd75ca94a7b9e4282c8
sakamoto <mossan@fout.jp>
2016-04-16 13:08:05 +0900

    create fabfile

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+from __future__ import unicode_literals
+from fabric.api import *
+from d import setting_secret as settings
+
+env.hosts = settings.DEPLOY_HOSTS
+env.key_filename = ['~/.ssh/mcsk_rsa']
+env.user = 'mcsk'
+
+
+def say_hostname():
+    run('hostname')
